13dcef510cb35d5819e0a956481a5ddf75933f7a
alevochkin <alexl@geomotiv.com>
2015-08-12 17:21:56 +0300

    Fast first implementation

--- collinear/src/main/java/Brute.java
+++ collinear/src/main/java/Brute.java
@@ -4,38 +4,32 @@ import java.util.Arrays;
  * @author alevochkin.
  */
 public class Brute {
+    private static final String POINT_SEPARATOR = "->";
+
     public static void main(String[] args) {
         if (args.length < 1) {
-            throw new RuntimeException("Please enter inout file");
+            throw new RuntimeException("Please enter input file");
         }
         In in = new In(args[0]);
-        int number = in.readInt();
-        Point[] points = new Point[number];
+        Point[] points = new Point[in.readInt()];
 
         StdDraw.setXscale(0, 32768);
         StdDraw.setYscale(0, 32768);
 
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < points.length; i++) {
             Point point = new Point(in.readInt(), in.readInt());
             points[i] = point;
             point.draw();
         }
 
-        for (int p = 0; p < number; p++) {
-            for (int q = p + 1; q < number; q++) {
+        for (int p = 0; p < points.length; p++) {
+            for (int q = p + 1; q < points.length; q++) {
                 double slopeToQ = points[p].slopeTo(points[q]);
-                for (int r = q + 1; r < number; r++) {
-                    double slopeToR = points[p].slopeTo(points[r]);
-                    if (slopeToQ == slopeToR) {
-                        for (int s = r + 1; s < number; s++) {
-                            double slopToS = points[p].slopeTo(points[s]);
-                            if (slopeToQ == slopToS) {
-                                Point[] line = new Point[4];
-                                line[0] = points[p];
-                                line[1] = points[q];
-                                line[2] = points[r];
-                                line[3] = points[s];
-                                printLine(line);
+                for (int r = q + 1; r < points.length; r++) {
+                    if (slopeToQ == points[p].slopeTo(points[r])) {
+                        for (int s = r + 1; s < points.length; s++) {
+                            if (slopeToQ == points[p].slopeTo(points[s])) {
+                                printLine(new Point[] {points[p], points[q], points[r], points[s]});
                             }
                         }
                     }
@@ -49,11 +43,10 @@ public class Brute {
         StringBuilder builder = new StringBuilder();
         for (Point point : line) {
             builder.append(point);
-            builder.append("->");
+            builder.append(POINT_SEPARATOR);
         }
-        builder.setLength(builder.length() - 2);
+        builder.setLength(builder.length() - POINT_SEPARATOR.length());
         StdOut.println(builder.toString());
         line[0].drawTo(line[3]);
-        //Arrays.min(line).drawTo(Arrays.max(line));
     }
 }
